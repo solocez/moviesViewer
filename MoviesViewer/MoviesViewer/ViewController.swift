01826c09103d3771d5904c28f5ddb4f6a0f2f4e5
solocez <solocez@gmail.com>
2019-04-10 13:25:41 +0300

    Initial commit

---
+++ MoviesViewer/MoviesViewer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MoviesViewer
+//
+//  Created by Zakhar Sukhanov on 2019-04-10.
+//  Copyright © 2019 solocez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
